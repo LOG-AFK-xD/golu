662583847ab50f98c0ca0575ddb2d6f47057b641
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-01-11 18:10:16 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -71,19 +71,19 @@ async def start_(client: Client, message: Message):
                 [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
                 [
                     InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
-                    InlineKeyboardButton("❤️ Donate", url=f"https://t.me/{OWNER_NAME}"),
+                    InlineKeyboardButton("❤️ Owner", url=f"https://t.me/TabahiOwnerGoluOp"),
                 ],
                 [
                     InlineKeyboardButton(
-                        "👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}"
+                        "💫 Official Group", url=f"https://t.me/SabkabaapTabahiOp"
                     ),
                     InlineKeyboardButton(
-                        "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                        "Official Channel ✨", url=f"https://t.me/Patanjalimod_0"
                     ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 Source Code", url="https://github.com/levina-lab/video-stream"
+                        "❣️ Chat Zone ❣️", url="https://t.me/Turulove_xd"
                     )
                 ],
             ]
